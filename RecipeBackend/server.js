de8e95f94e6fccd93f3e5a38e18c02ad63cce461
VISH01AL <g.vish1al@gmail.com>
2025-02-24 19:48:29 -0500

    added workign backend

--- RecipeBackend/server.js
+++ RecipeBackend/server.js
@@ -13,14 +13,21 @@ const {sequelize}=require("./models");
 
 app.use(express.json());
 app.use(cors())
-
+app.get('/', (req, res) => {
+    res.sendStatus(200);  // Root health check for load balancer
+  });
+  app.get('/healthz', (req, res) => {
+    res.sendStatus(200);  // Additional readiness probe endpoint
+  });
 
 app.use(process.env.PREFIX+"/",chefRouter)
 app.use(process.env.PREFIX+"/protected",chefProtectorMiddleware)
 app.use(process.env.PREFIX+"/protected",recipeRouter)
 app.use(process.env.PREFIX+"/protected",chefProfileRouter)
 
-
+app.use((req, res, next) => {
+    res.status(404).json({ error: "Path not found" });
+});
 
 
 app.listen(process.env.PORT||8080,async()=>{
